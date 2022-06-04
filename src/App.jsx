6e27ac92793e7a7676054a2e6cc7467b576c40c7
skyxcripto <producer@skynance.net>
2022-06-04 14:17:39 -0300

    feat: header

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-import Post from './component'
+import {Header} from './components/Header';
+import "./styles/global.css"
 
 function App() {
 
-  const post = () => {
-    return (
-      <>
-        <Post title="Ola testando... " idade={22} teste="teste" ola="ola" />
-        <Post title="Ola testando 2 ... " idade={22} teste="234324" ola="cs"/>
-        <Post title="Ola testando 43... " idade={22} teste="fsfs" ola="vh" />
-      </>
-    )
-  }
   return (
     <div className="App">
-      {post()}
+      <Header />
     </div>
   )
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,13 @@
 import styles from './Header.module.css';
 
+import logo from '../../assets/images/logo.svg';
+
 export function Header() {
 
 
   return (
-    <header className={styles.header}>
-      <strong>CodeFeeds</strong>
+    <header className={styles.headerContainer}>
+      <img src={logo} alt="logo" className={styles.logo} />
      </header>
   )
 }
