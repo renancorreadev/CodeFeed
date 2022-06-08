625a772cc82f4b19ebfecf0c69088a9455b7e1ca
skyxcripto <producer@skynance.net>
2022-06-08 00:31:29 -0300

    feat: like comment

--- src/components/Post/Comment/index.jsx
+++ src/components/Post/Comment/index.jsx
@@ -1,11 +1,19 @@
+import {useState} from 'react'
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import {Avatar} from '../../Avatar'
 
+
 export function Comment({content, onDeleteComment}) {
+
+    const [likeCount, setLikeCount] = useState(0);
 
     const handleDeleteComment = () => {
         onDeleteComment(content);
+    }
+
+    const handleLikeComment = () => {
+        setLikeCount(likeCount+1);
     }
 
     return (
@@ -33,9 +41,9 @@ export function Comment({content, onDeleteComment}) {
                 </div>
 
                 <footer>
-                    <button >
+                    <button onClick={handleLikeComment} >
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
